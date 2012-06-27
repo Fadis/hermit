35fb010bd94e63d1678033aa5d20a88468a4464c
Fadis <fadis@quaternion.sakura.ne.jp>
2012-06-27 22:25:46 +0900

    ある型Tがpod型だった場合でもエラーにならないようにする

--- src/has_memfun_xxx_sample.cpp
+++ src/has_memfun_xxx_sample.cpp
@@ -17,9 +17,13 @@
 HPP_MEMFUN_XXX( assign )
    
 class Moo {};
+class Woo {
+  void assign( float*, float* );
+};
  
 int main() {
   std::cout << detect_memfun_assign< std::vector< int >, void( int*, int* ) >::value << std::endl;
   std::cout << detect_memfun_assign< Moo, void( int*, int* ) >::value << std::endl;
+  std::cout << detect_memfun_assign< Moo, void( float*, float* ) >::value << std::endl;
   std::cout << detect_memfun_assign< int, void( int*, int* ) >::value << std::endl;
 }
