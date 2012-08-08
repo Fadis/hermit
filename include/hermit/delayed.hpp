909c97e5a75b16979cee3761c63cd8bdec8d9e10
fadis <fadis@Grueza.(none)>
2012-08-08 09:17:15 +0900

    delayedが正しく動いていなかったのを修正

--- include/hermit/delayed.hpp
+++ include/hermit/delayed.hpp
@@ -21,7 +21,7 @@
 
 namespace hermit {
   template< typename T >
-    class delayed {
+    class delayed_internal {
       public:
         typedef
 #ifdef BOOST_NO_0X_HDR_FUTURE
@@ -31,18 +31,21 @@ namespace hermit {
 #endif
         promise;
         typedef
+#ifdef BOOST_NO_0X_HDR_FUTURE
+          boost::unique_future< T >
+#else
+          std::future< T >
+#endif
+        future;
+        typedef
 #ifdef BOOST_NO_0X_HDR_MUTEX
           boost::mutex
 #else
           std::mutex
 #endif
         mutex;
-        typedef std::shared_ptr< promise > promise_ptr;
-        delayed() : value( promise_ptr( new promise ) ), guard( new mutex ) {
-          dereference = std::bind( &delayed<T>::initial_access, this );
-        }
-        T &operator*() {
-          return dereference();
+        delayed_internal( std::shared_ptr< promise > value_ ) : value( value_ ) {
+          dereference = std::bind( &delayed_internal<T>::initial_access, this );
         }
         const T &operator*() const {
           return dereference();
@@ -50,33 +53,47 @@ namespace hermit {
         operator T() const {
           return dereference();
         }
-        promise_ptr get_promise() {
-#ifdef BOOST_NO_0X_HDR_MUTEX
-          boost::mutex::scoped_lock lock( *guard );
-#else
-          std::lock_guard< std::mutex > lock( *guard );
-#endif
-          return boost::get< promise_ptr >( value );
-        }
       private:
-        T &initial_access() {
+        const T &initial_access() {
 #ifdef BOOST_NO_0X_HDR_MUTEX
-          boost::mutex::scoped_lock lock( *guard );
+          boost::mutex::scoped_lock lock( guard );
 #else
-          std::lock_guard< std::mutex > lock( *guard );
+          std::lock_guard< std::mutex > lock( guard );
 #endif
           if( value.which() == 0 ) {
-            value = boost::get< promise_ptr >( value )->get_future().get();
-            dereference = std::bind( &delayed<T>::cached_access, this );
+            value = boost::get< std::shared_ptr< promise > >( value )->get_future().get();
+            dereference = std::bind( &delayed_internal<T>::cached_access, this );
           }
           return boost::get< T >( value );
         }
-        T &cached_access() {
+        const T &cached_access() {
           return boost::get< T >( value );
         }
-        mutable std::function< T&() > dereference;
-        boost::variant< promise_ptr, T > value;
-        boost::shared_ptr< mutex > guard;
+        mutable std::function< const T&() > dereference;
+        boost::variant< std::shared_ptr< promise >, T > value;
+        mutex guard;
+    };
+
+  template< typename T >
+    class delayed {
+      public:
+        typedef
+#ifdef BOOST_NO_0X_HDR_FUTURE
+          boost::promise< T >
+#else
+          std::promise< T >
+#endif
+        promise;
+        delayed( std::shared_ptr< promise > value_ ) : internal( new delayed_internal< T >( value_ ) ) {
+        }
+        const T &operator*() const {
+          return **internal;
+        }
+        operator T() const {
+          return **internal;
+        }
+      private:
+        std::shared_ptr< delayed_internal< T > > internal;
     };
 }
 
--- src/delayed_sample.cpp
+++ src/delayed_sample.cpp
@@ -5,11 +5,11 @@
 #include <unistd.h>
 
 int main() {
-  hermit::delayed< int > d;
-  auto promise = d.get_promise();
+  std::shared_ptr< hermit::delayed< int >::promise > promise( new hermit::delayed< int >::promise() );
+  hermit::delayed< int > d( promise );
   std::thread bg( [promise](){ sleep( 1 ); promise->set_value( 3 );} );
-  std::cout << d << std::endl;
-  std::cout << d << std::endl;
-  std::cout << d << std::endl;
+  std::cout << *d << std::endl;
+  std::cout << *d << std::endl;
+  std::cout << *d << std::endl;
   bg.join();
 }
