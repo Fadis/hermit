588992c4e5a490e4913042f238330be73ee5046b
Fadis <fadis@quaternion.sakura.ne.jp>
2012-08-11 15:57:35 +0900

    shared_ptrの中身が突然消える謎現象追跡中

--- src/task_remapper_poll_sample.cpp
+++ src/task_remapper_poll_sample.cpp
@@ -1,10 +1,16 @@
 #include <hermit/task_remapper.hpp>
 #include <unistd.h>
+#include <boost/optional.hpp>
+
+hermit::poller hoge( hermit::task_remapper &sched ) {
+  return sched.post( [](){ std::cout << "foo" << std::endl; }, boost::posix_time::milliseconds(200) );
+}
 
 int main() {
   hermit::task_remapper sched( 1 );
-  auto foo = sched.post( [](){ std::cout << "foo" << std::endl; }, boost::posix_time::milliseconds(200) );
-  auto bar = sched.post( [](){ std::cout << "bar" << std::endl; }, boost::posix_time::milliseconds(300) );
-  sleep( 5 );
+  boost::optional< hermit::poller > foo = hoge( sched );
+  sleep( 1 );
+  foo = boost::optional< hermit::poller >(); 
+  sleep( 1 );
 }
 
