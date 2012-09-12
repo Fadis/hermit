8ece18775d150100b6755e35fa096a85fa3ab3e0
Fadis <fadis@quaternion.sakura.ne.jp>
2012-09-12 23:21:14 +0900

    MessagePackテスト中

--- src/json_sample.cpp
+++ src/json_sample.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <cstdint>
 #include <hermit/json.hpp>
-
+#include <hermit/messagepack.hpp>
 
 int main() {
+  hermit::messagepack::rule< std::string::iterator > mp;
   std::string sample;
   sample.assign( std::istream_iterator<char>( std::cin ), std::istream_iterator<char>() );
   const auto decoded = hermit::json::parse( sample );
