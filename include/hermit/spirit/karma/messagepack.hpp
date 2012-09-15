82c9354bfae7c381696c9f83f1aecd8cf83351e1
Fadis <fadis@quaternion.sakura.ne.jp>
2012-09-15 20:20:54 +0900

    不要になったコードを削除

--- include/hermit/spirit/karma/messagepack.hpp
+++ include/hermit/spirit/karma/messagepack.hpp
@@ -59,45 +59,39 @@ namespace hermit {
           double_ = karma::byte_( 0xcb ) << karma::big_bin_double;
           raw_short = karma::byte_[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0x1F,
-            karma::_1 = 0xC0|boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ karma::byte_ ][
+            karma::_1 = 0xC0|boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ karma::byte_ ][
             karma::_1 = karma::_val
           ];
           raw_16 = ( karma::byte_( 0xda ) << karma::big_word )[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFF,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ karma::byte_ ][
+            karma::_1 = boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ karma::byte_ ][
             karma::_1 = karma::_val
           ];
           raw_32 = ( karma::byte_( 0xdb ) << karma::big_dword )[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFFFFFFul,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ karma::byte_ ][
+            karma::_1 = boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ karma::byte_ ][
             karma::_1 = karma::_val
           ];
           raw = raw_short|raw_16|raw_32;
           array_short = karma::byte_[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0x0F,
-            karma::_1 = 0x90|boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ value_ ][
+            karma::_1 = 0x90|boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ value_ ][
             karma::_1 = karma::_val
           ];
           array_16 = ( karma::byte_( 0xdc ) << karma::big_word )[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFF,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ value_ ][
+            karma::_1 = boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ value_ ][
             karma::_1 = karma::_val
           ];
           array_32 = ( karma::byte_( 0xdd ) << karma::big_dword )[
             karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFFFFFFul,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ value_ ][
+            karma::_1 = boost::phoenix::size( karma::_val )
+          ] << karma::repeat( boost::phoenix::size( karma::_val ) )[ value_ ][
             karma::_1 = karma::_val
           ];
           array = array_short|array_16|array_32;
@@ -116,25 +110,7 @@ namespace hermit {
           ];
           map_data = karma::repeat( boost::phoenix::size( karma::_val ) )[ named_value ];
           map = ( ( map_short|map_16|map_32 ) << map_data )[ karma::_1 = karma::_val, karma::_2 = karma::_val ];
-          /*<< karma::repeat( karma::_a )[ named_value ][
-            karma::_1 = 1, karma::_2 = 2
-          ];*/
-/*          map_16 = ( karma::byte_( 0xde ) << karma::big_word )[
-            karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFF,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ named_value ][
-            karma::_1 = karma::_val
-          ];
-          map_32 = ( karma::byte_( 0xdf ) << karma::big_dword )[
-            karma::_pass = boost::phoenix::size( karma::_val ) <= 0xFFFFFFFFul,
-            karma::_1 = boost::phoenix::size( karma::_val ),
-            karma::_a = boost::phoenix::size( karma::_val )
-          ] << karma::repeat( karma::_a )[ named_value ][
-            karma::_1 = karma::_val
-          ];*/
- //         map = map_short|map_16|map_32;
- //         value_ = uint8_|uint16_|uint32_|uint64_|int8_|int16_|int32_|int64_|null_|bool_|float_|double_|raw|array|map;
+          value_ = uint8_|uint16_|uint32_|uint64_|int8_|int16_|int32_|int64_|null_|bool_|float_|double_|raw|array|map;
       }
       private: 
         boost::spirit::karma::rule< Iterator, uint8_t() > uint8_short;
@@ -153,20 +129,20 @@ namespace hermit {
         boost::spirit::karma::rule< Iterator, bool() > bool_;
         boost::spirit::karma::rule< Iterator, float() > float_;
         boost::spirit::karma::rule< Iterator, double() > double_;
-        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >(), boost::spirit::karma::locals< uint32_t > > raw_short;
-        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >(), boost::spirit::karma::locals< uint32_t > > raw_16;
-        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >(), boost::spirit::karma::locals< uint32_t > > raw_32;
+        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >() > raw_short;
+        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >() > raw_16;
+        boost::spirit::karma::rule< Iterator, std::vector< uint8_t >() > raw_32;
         boost::spirit::karma::rule< Iterator, std::vector< uint8_t >() > raw;
-        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > array_short;
-        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > array_16;
-        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > array_32;
-        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > array;
+        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >() > array_short;
+        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >() > array_16;
+        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >() > array_32;
+        boost::spirit::karma::rule< Iterator, std::vector< hermit::messagepack >() > array;
         boost::spirit::karma::rule< Iterator, std::pair< hermit::messagepack, hermit::messagepack >() > named_value;
-        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > map_short;
-        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > map_data;
-        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > map_16;
-        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > map_32;
-        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >(), boost::spirit::karma::locals< uint32_t > > map;
+        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >() > map_short;
+        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >() > map_data;
+        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >() > map_16;
+        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >() > map_32;
+        boost::spirit::karma::rule< Iterator, std::map< hermit::messagepack, hermit::messagepack >() > map;
         boost::spirit::karma::rule< Iterator, hermit::messagepack() > value_;
       };
     }
