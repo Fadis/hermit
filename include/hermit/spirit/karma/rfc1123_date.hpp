38d941718e27f3ea8232733a0b584e88346f4bc0
Fadis <fadis@quaternion.sakura.ne.jp>
2012-10-10 23:30:05 +0900

    RFC1123とRFC850のdate-timeのジェネレータを実装

--- include/hermit/spirit/karma/rfc1123_date.hpp
+++ include/hermit/spirit/karma/rfc1123_date.hpp
@@ -1,5 +1,5 @@
-#ifndef HERMIT_SPIRIT_KARMA_RFC850_DATE_HPP
-#define HERMIT_SPIRIT_KARMA_RFC850_DATE_HPP
+#ifndef HERMIT_SPIRIT_KARMA_RFC1123_DATE_HPP
+#define HERMIT_SPIRIT_KARMA_RFC1123_DATE_HPP
 
 #include <iostream>
 #include <vector>
@@ -15,38 +15,36 @@ namespace hermit {
   namespace spirit {
     namespace karma {
       template <typename InputIterator>
-        class rfc850_date :
+        class rfc1123_date :
           public boost::spirit::karma::grammar< InputIterator, boost::posix_time::ptime() > {
             public:
-              rfc850_date() : rfc850_date::base_type( root ) {
+              rfc1123_date() : rfc1123_date::base_type( root ) {
                 namespace karma = boost::spirit::karma;
                 namespace dt = boost::date_time;
                 namespace phx = boost::phoenix;
-                root = ( weekday << ", " << date << ' ' << time << " GMT" )[
+                root = ( wkday << ", " << date << ' ' << time << " GMT" )[
                   karma::_2 = phx::bind( &boost::posix_time::ptime::date, &karma::_val ),
                   karma::_1 = phx::bind( &boost::gregorian::greg_weekday::as_enum,
                     phx::bind( &boost::gregorian::date::day_of_week, &karma::_2 ) ),
                   karma::_3 = phx::bind( &boost::posix_time::ptime::time_of_day, &karma::_val )
                 ];
-                date = ( -karma::char_ << dec_p << '-' << month << '-' << dec_p )[
-                  karma::_pass = phx::bind( &boost::gregorian::date::year, &karma::_val ) < 2000 &&
-                    phx::bind( &boost::gregorian::date::year, &karma::_val ) >= 1900,
+                date = ( -karma::char_ << dec_p << ' ' << month << ' ' << dec_p )[
                   karma::_2 = phx::bind( &boost::gregorian::date::day, &karma::_val ),
                   phx::if_( karma::_2 < 10ul )[
                     karma::_1 = '0'
                   ],
                   karma::_3 = phx::bind( &boost::gregorian::greg_month::as_enum,
                     phx::bind( &boost::gregorian::date::month, &karma::_val ) ),
-                  karma::_4 = phx::bind( &boost::gregorian::date::year, &karma::_val ) - 1900
+                  karma::_4 = phx::bind( &boost::gregorian::date::year, &karma::_val )
                 ];
                 time = ( dec_p << ':' << dec_p << ':' << dec_p )[
                   karma::_1 = phx::bind( &boost::posix_time::time_duration::hours, &karma::_val ),
                   karma::_2 = phx::bind( &boost::posix_time::time_duration::minutes, &karma::_val ),
                   karma::_3 = phx::bind( &boost::posix_time::time_duration::seconds, &karma::_val )
                 ];
-                weekday.add( dt::Monday, "Monday" )( dt::Tuesday, "Tuesday" )( dt::Wednesday, "Wednesday" )
-                           ( dt::Thursday, "Thursday" )( dt::Friday, "Friday" )( dt::Saturday, "Saturday" )
-                           ( dt::Sunday, "Sunday" );
+                wkday.add( dt::Monday, "Mon" )( dt::Tuesday, "Tue" )( dt::Wednesday, "Wed" )
+                           ( dt::Thursday, "Thu" )( dt::Friday, "Fri" )( dt::Saturday, "Sat" )
+                           ( dt::Sunday, "Sun" );
                 month.add( dt::Jan, "Jan" )( dt::Feb, "Feb" )( dt::Mar, "Mar" )( dt::Apr, "Apr" )
                          ( dt::May, "May" )( dt::Jun, "Jun" )( dt::Jul, "Jul" )( dt::Aug, "Aug" )
                          ( dt::Sep, "Sep" )( dt::Oct, "Oct" )( dt::Nov, "Nov" )( dt::Dec, "Dec" );
@@ -59,7 +57,7 @@ namespace hermit {
               boost::spirit::karma::rule< InputIterator, boost::posix_time::time_duration() > zone;
               boost::spirit::karma::rule< InputIterator, boost::posix_time::time_duration() > zone_pos;
               boost::spirit::karma::rule< InputIterator, boost::posix_time::time_duration() > zone_neg;
-              boost::spirit::karma::symbols< boost::date_time::weekdays, std::string > weekday;
+              boost::spirit::karma::symbols< boost::date_time::weekdays, std::string > wkday;
               boost::spirit::karma::symbols< boost::date_time::months_of_year, std::string > month;
       };
     }
--- src/date_time_sample.cpp
+++ src/date_time_sample.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <hermit/spirit/qi/http_date.hpp>
 #include <hermit/spirit/karma/rfc850_date.hpp>
+#include <hermit/spirit/karma/rfc1123_date.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 int main() {
@@ -14,7 +15,13 @@ int main() {
       boost::spirit::qi::parse( iter, source.end(), rule, decoded );
       std::cout << std::distance( source.begin(), iter ) << std::endl;
     }
-    std::cout << decoded << std::endl;
+    std::string encoded;
+    {
+      typedef std::back_insert_iterator<std::string> sink_type;
+      hermit::spirit::karma::rfc1123_date< sink_type > rule;
+      boost::spirit::karma::generate( sink_type( encoded ), rule, decoded );
+    }
+    std::cout << encoded << std::endl;
   }
   {
     std::string source( "Sunday, 06-Nov-94 08:49:37 GMT" );
@@ -26,6 +33,13 @@ int main() {
       std::cout << std::distance( source.begin(), iter ) << std::endl;
     }
     std::cout << decoded << std::endl;
+    std::string encoded;
+    {
+      typedef std::back_insert_iterator<std::string> sink_type;
+      hermit::spirit::karma::rfc850_date< sink_type > rule;
+      boost::spirit::karma::generate( sink_type( encoded ), rule, decoded );
+    }
+    std::cout << encoded << std::endl;
   }
   {
     std::string source( "Sun Nov  6 08:49:37 1994" );
